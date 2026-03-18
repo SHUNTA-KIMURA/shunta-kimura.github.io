4659820860d4d92cebe8cadd22041250b07e136a
SHUNTA-KIMURA <45725330+SHUNTA-KIMURA@users.noreply.github.com>
2026-03-18 08:53:06 +0000

    Deploying to gh-pages from @ SHUNTA-KIMURA/shunta-kimura.github.io@a1127bee5f4b9f293705c7a5e92aa780c02c7934 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-curriculum-vitae",
-          title: "Curriculum Vitae",
+        },{id: "nav-cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
